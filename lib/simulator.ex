68ced755357108ffb01eb85f16e162dec3179b05
elixirprogrammer <anthony@elixirprogrammer.com>
2022-05-14 13:01:00 -0400

    adds comments to simulator functions

--- lib/simulator.ex
+++ lib/simulator.ex
@@ -22,6 +22,19 @@ defmodule Simulator do
     positions <> "\n"
   end
 
+  # Converts input to list chunked by 2 removing first position of list
+  # Position 0 at each chunked list inside is the initial position and at the second position is the instructions.
+  # Examples:
+  # iex> input = """
+  # 5 5
+  # 1 2 N
+  # LMLMLMLMM
+  # 3 3 E
+  # MMRMMRMRRM
+  # """
+  # iex> chunk_input_string(input)
+  # [["1 2 N", "LMLMLMLMM"], ["3 3 E", "MMRMMRMRRM"]]
+  @spec chunk_input_string(input :: bitstring()) :: list()
   defp chunk_input_string(input) do
     input
     |> String.split("\n", trim: true)
@@ -29,6 +42,22 @@ defmodule Simulator do
     |> Enum.chunk_every(2)
   end
 
+  # Loops through each chunked list and gets a map with initial position and
+  # coordinates, then returns a list of maps
+  # Examples:
+  # iex> chunks = [["1 2 N", "LMLMLMLMM"], ["3 3 E", "MMRMMRMRRM"]]
+  # iex> get_instructions(chunks)
+  # [
+  #   %{
+  #     initial_position: %{x: 1, y: 2, direction: "N"},
+  #     commands: ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
+  #   },
+  #   %{
+  #     initial_position: %{x: 3, y: 3, direction: "E"},
+  #     commands: ["M", "M", "R", "M", "M", "R", "M", "R", "R", "M"]
+  #   }
+  # ]
+  @spec get_instructions(chunks :: list()) :: list()
   defp get_instructions(chunks) do
     chunks
     |> Enum.map(fn chunk ->
@@ -36,6 +65,18 @@ defmodule Simulator do
     end)
   end
 
+  # Takes a chunked list with the initial position and instructions
+  # Maps the initial position with x, y, coordinates and direction
+  # Returns a map with initial position coordinates and instructions or commands
+  # Commands is a list with all instructions
+  # Examples:
+  # iex> chunk = ["3 3 E", "MMRMMRMRRM"]
+  # iex> map_instructions(chunk)
+  # %{
+  #  initial_position: %{x: 3, y: 3, direction: "E"},
+  #  commands: ["M", "M", "R", "M", "M", "R", "M", "R", "R", "M"]
+  # }
+  @spec map_instructions(chunk :: list()) :: map()
   defp map_instructions(chunk) do
     initial_position = List.first(chunk)
     commands = List.last(chunk)
